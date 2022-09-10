1e29c66e1f4b516a3c2140c83c8bdb555afbda35
0wen99 <owenochieng99@gmail.com>
2022-09-10 04:19:56 -0700

    initial

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+		int n;
+			char ch;
+
+				for (n = 48; n < 58; n++)
+						{
+									putchar(n);
+										}
+					for (ch = 'a'; ch <= 'f'; ch++)
+							{
+										putchar(ch);
+											}
+						putchar('\n');
+							return (0);
+}
