43f5b14f0ffbde9ac7d4c50886c617ed59a5dc9d
owenochieng <owenoching99@gmail.com>
2022-09-15 02:52:18 +0300

    initial

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,13 +10,13 @@
 
 int main(void)
 {
-	char text[7] = "_putchar";
+	char text[9] = "_putchar";
 	int i = 0;
 
-	for (i = 0; i < 7; i++)
-	{
+	for (i = 0; i < 8; i++)
+	
 		_putchar(text[i]);
-	}
+	
 	_putchar('\n');
 
 	return (0);
