f339c95e1e563e8ffcb1344ba120a7a265832a74
owenochieng <owenoching99@gmail.com>
2022-09-15 04:07:30 +0300

    initial

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase
+ * @c: c is an ascii character
+ *
+ * Returns 1 if c is lowercase
+ * Returns 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
