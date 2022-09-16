07ec517ce93b6945d5e8ab638e16fa5d5e31a27b
owenochieng <owenoching99@gmail.com>
2022-09-17 01:26:36 +0300

    initial

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer
+ *
+ * @n: integer
+ *
+ */
+
+void print_number(int n)
+{
+	unsigned int o, p, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		o = n * -1;
+	}
+	else
+	{
+		o = n;
+	}
+
+	p = o;
+	count = 1;
+
+	while (p > 9)
+	{
+		p /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
+}
+
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - from 0 to 9
+ * Dont print 2 and 4
+ *
+ * Return 0
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
+	_putchar('\n');
+}
+
