279a09da3655174b4b53781992d33fbb2c49f2c7
owenochieng <owenoching99@gmail.com>
2022-09-17 03:12:28 +0300

    initial

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main -This file must test that the function positive_or_negative()
+* main - tests function that prints if integer is positive or negative
 *
 * Return: 0
 *
