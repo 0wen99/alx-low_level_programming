7e4c416a155d844d332e4c33e75ef041ff62ab6b
owenochieng <owenoching99@gmail.com>
2022-09-21 03:08:11 +0300

    initial

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 -takes a pointer and updates the value
+ *
+ * @n: pointer
+ *
+ *  Return: no return.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
