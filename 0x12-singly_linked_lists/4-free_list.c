4804382903bf129d1c9ef139efb5c494c93db238
owenochieng <owenoching99@gmail.com>
2022-10-25 01:57:50 +0300

    initial

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ *
+ * Return:0
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
+
